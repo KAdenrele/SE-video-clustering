75f122f4123a72b8d19647810ad5ecdfc9b13757
KAdenrele <akomolafe@zaizi.com>
2026-03-13 21:28:29 +0000

    added build to script

--- run_container.sh
+++ run_container.sh
@@ -1,10 +1,10 @@
-docker run --rm -it \
-  --name video-clustering-container \
-  --gpus "device=0" \
-  --shm-size=8g \
-  --env-file .env \
-  -v /mnt/data3/video_clustering/videos:/workspace/video_data \
-  -v /mnt/data/test_dataset/raw:/workspace/hf_cache \
-  -v /home/ade/SE-video-clustering:/workspace/video_cluster \
-  -v /mnt/data3/video_clustering/models:/workspace/models \
-  video_clustering
+docker build -t video_clustering . && docker run --rm -it \
+                                                  --name video-clustering-container \
+                                                  --gpus "device=0" \
+                                                  --shm-size=8g \
+                                                  --env-file .env \
+                                                  -v /mnt/data3/video_clustering/videos:/workspace/video_data \
+                                                  -v /mnt/data/test_dataset/raw:/workspace/hf_cache \
+                                                  -v /home/ade/SE-video-clustering:/workspace/video_cluster \
+                                                  -v /mnt/data3/video_clustering/models:/workspace/models \
+                                                  video_clustering
